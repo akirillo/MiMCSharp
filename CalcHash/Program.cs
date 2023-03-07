7608d2931a7566883b901c48ad0ad3dd81d27151
Andrew Kirillov <20803092+akirillo@users.noreply.github.com>
2023-03-07 15:33:34 -0500

    matches gnark MiMC 🤩

--- CalcHash/Program.cs
+++ CalcHash/Program.cs
@@ -1,4 +1,6 @@
 ﻿using MimcLibraries;
+using Org.BouncyCastle.Math.EC;
+using Org.BouncyCastle.Math;
 
 class Program
 {
@@ -6,8 +8,11 @@ class Program
     {
         // LETS BITE THE BULLET EARLY AND BE SURE WE HAVE ROUND CONSTANT
         // EQUIVALNCE W/ GNARK FROM THE SAME SEED STRING
-        var mimc = new MimcBN254(numRounds: 1);
-        var roundConstants = mimc.GetRoundConstants();
-        Console.WriteLine($"roundConstants: {roundConstants}");
+        var mimc = new MimcBN254(numRounds: 5);
+        mimc.Write(new List<ECFieldElement>(){
+            MimcBN254.BN254Curve.FromBigInteger(new BigInteger("1"))
+        });
+        var res = mimc.Sum();
+        Console.WriteLine(res.ToBigInteger().ToString());
     }
 }
--- MimcBN254/MimcBN254.cs
+++ MimcBN254/MimcBN254.cs
@@ -1,22 +1,38 @@
 ﻿namespace MimcLibraries;
-using System.Numerics;
 using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Math.EC;
+using Org.BouncyCastle.Math;
 using System.Text;
 
 public class MimcBN254
 {
-    private BigInteger babyJubJub = BigInteger.Parse("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+    // Don't really need to use the curve, just need it to generate FpFieldElements
+    // This feels like a huge can of worms...
+    public static FpCurve BN254Curve = new FpCurve(
+        new BigInteger("21888242871839275222246405745257275088548364400416034343698204186575808495617"),
+        BigInteger.Zero,
+        BigInteger.Zero,
+        BigInteger.Zero,
+        BigInteger.Zero
+    );
+
+    private static ECFieldElement initialHashState = MimcBN254.BN254Curve.FromBigInteger(BigInteger.Zero);
+
     private int numRounds;
-    private BigInteger[] roundConstants;
-    private BigInteger hashState;
-    private BigInteger[]? input;
+
+    private ECFieldElement[] roundConstants;
+
+    private ECFieldElement hashState;
+
+    private List<ECFieldElement> input;
 
     public MimcBN254(string seed = "seed", int numRounds = 110) {
-        this.numRounds = numRounds;
-        this.roundConstants = new BigInteger[this.numRounds];
+        this.input = new List<ECFieldElement>();
+        this.hashState = MimcBN254.initialHashState;
 
-        // Calculate round constants using Keccak256
-        // Hoping this is the same padding scheme as what is used in gnark...
+        this.numRounds = numRounds;
+        this.roundConstants = new ECFieldElement[this.numRounds];
+
         var digest = new KeccakDigest(256);
         var bseed = Encoding.UTF8.GetBytes(seed);
         digest.BlockUpdate(bseed, 0, bseed.Length);
@@ -27,18 +43,45 @@ public class MimcBN254
 
         for(var i = 0; i < this.numRounds; i++) {
             digest.DoFinal(rnd, 0);
-            // Parses byte[] rnd in big-endian order
-            // TODO: FFA library to take modulo here? Oh god...
-            // LOOK INTO BOUNCYCASTLE ECFIELDELEMENT
-            var roundConstant = new BigInteger(rnd, true, true);
-            Console.WriteLine($"round constant {i}: {roundConstant}");
+            // Parses byte[] rnd as big-endian unsigned integer
+            var roundConstant = MimcBN254.BN254Curve.FromBigInteger(
+                new BigInteger(1, rnd).Mod(MimcBN254.BN254Curve.Q)
+            );
             this.roundConstants[i] = roundConstant;
             digest.Reset();
             digest.BlockUpdate(rnd, 0, rnd.Length);
         }
     }
 
-    public BigInteger[] GetRoundConstants() {
-        return this.roundConstants;
+    public void Write(List<ECFieldElement> data) {
+        var newInput = new List<ECFieldElement>(this.input.Count + data.Count);
+        newInput.AddRange(this.input);
+        newInput.AddRange(data);
+        this.input = newInput;
+    }
+
+    public void Reset() {
+        this.input.Clear();
+        this.hashState = MimcBN254.initialHashState;
+    }
+
+    // Calculates MiMCHash-n/n using Miyaguchi-Preneel one-way compression scheme
+    public ECFieldElement Sum() {
+        foreach(ECFieldElement e in this.input) {
+            var r = this.encryptPow5(e);
+            r = r.Add(e);
+            this.hashState = this.hashState.Add(r);
+        }
+        this.input.Clear();
+        return this.hashState;
+    }
+
+    private ECFieldElement encryptPow5(ECFieldElement e) {
+        var x = e;
+        foreach(ECFieldElement roundConstant in this.roundConstants) {
+            x = x.Add(this.hashState).Add(roundConstant);
+            x = x.Square().Square().Multiply(x);
+        }
+        return x.Add(this.hashState);
     }
 }
